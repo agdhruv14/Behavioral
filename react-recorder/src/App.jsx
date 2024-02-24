81e9327b6a41c4d0d153a2c39515c6aa70a94135
agdhruv14 <agdhruv14@gmail.com>
2024-02-24 17:15:54 -0600

    added functional "play question"

--- react-recorder/src/App.jsx
+++ react-recorder/src/App.jsx
@@ -5,35 +5,13 @@ import AudioRecorder from "../src/AudioRecorder";
 const App = () => {
     const [recordOption, setRecordOption] = useState("audio");
 
-    const [data, setdata] = useState({
-        question: ""
-    });
-
-    // Using useEffect for single rendering
-    useEffect(() => {
-        // Using fetch to fetch the api from 
-        // flask server it will be redirected to proxy
-        fetch("http://127.0.0.1:8080/questions", {
-            mode: "cors"
-        }).then((res) =>
-            res.json().then((data) => {
-                // Setting a data from api
-                console.log("help")
-                setdata({
-                    question: data.Question
-                });
-            })
-        );
-    }, []);
-
 
     return (
         <div>
             <h1>Behavioral Interview Simulator</h1>
             <div>
                 <AudioRecorder />
-                <p>{data.Question}</p>    
-                <p>{question}</p>            
+                {/* <p>{data.question}</p>     */}
             </div>
         </div>
     );
